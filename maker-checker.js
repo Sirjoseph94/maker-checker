8c5674ada524bc59c61646f6939aced2d331f7c4
Joseph Ero <erojoseph94@gmail.com>
2024-04-05 21:59:46 +0100

    add JSDocs for better intellisence

--- maker-checker.js
+++ maker-checker.js
@@ -1,47 +1,81 @@
-"use strict";
-
+/**
+ * Represents the different types of requests.
+ * @readonly
+ * @enum {string}
+ */
 const TYPES = {
-  A: "A",
-  B: "B",
-  C: "C",
+  A: 'A',
+  B: 'B',
+  C: 'C'
 };
 
-
+/**
+ * Represents a user in the system.
+ */
 class User {
+  /**
+   * Creates a new User instance.
+   * @param {string} name - The name of the user.
+   * @param {string[]} types - The types associated with the user.
+   */
   constructor(name, types) {
     this.name = name;
     this.types = types;
   }
-  // store in DB
 }
 
+/**
+ * Represents a request in the system.
+ */
 class Request {
+  /**
+   * Creates a new Request instance.
+   * @param {string} requestType - The type of the request.
+   * @param {User} requester - The user who made the request.
+   */
   constructor(requestType, requester) {
     this.requestType = requestType;
     this.requester = requester;
-    this.expirationTime = new Date(Date.now() + 1000 * (60 * 5)); // expire in 5 mins
+    this.expirationTime = new Date(Date.now() + 1000 * (60 * 5)); // Expire in 5 minutes
   }
-  // store on db
+
+  /**
+   * Checks if the given date has passed the request's expiration time.
+   * @param {Date} date - The date to check.
+   * @returns {boolean} - True if the date has passed the request's expiration time, false otherwise.
+   */
   static isExpired(date) {
     return new Date() > date;
   }
 }
 
+/**
+ * Represents an approver for a request.
+ */
 class Approver {
+  /**
+   * Creates a new Approver instance.
+   * @param {Request} request - The request to be approved or denied.
+   * @param {User} approver - The user who is the approver.
+   */
   constructor(request, approver) {
     this.request = request;
     this.user = approver;
   }
 
+  /**
+   * Checks if the approver can approve the request.
+   * @returns {boolean} - True if the approver can approve the request, false otherwise.
+   */
   canApprove() {
-    if (Request.isExpired(this.request.expirationTime)) {
-      console.log('Exceeded expiration time')
+    if (new Date() > this.request.expirationTime) {
+      console.log('Exceed expiration time');
       return false;
     }
 
     if (this.request.requestType === TYPES.A) {
-      console.log(this.request.requestType)
-      console.log('Unauthorized')
+      console.log(this.request.requestType);
+      console.log('Unauthorized');
       return false;
     }
 
@@ -56,27 +90,32 @@ class Approver {
     return false;
   }
 
+  /**
+   * Approves the request if the approver can approve it.
+   * @returns {boolean} - True if the request was approved, false otherwise.
+   */
   approve() {
     if (!this.canApprove()) {
       return false;
     }
-    // update db
+
+    // Update the database
     return true;
   }
 
+  /**
+   * Denies the request if it has not expired.
+   * @returns {boolean} - True if the request was denied, false otherwise.
+   */
   deny() {
-    console.log(this.request.expirationTime)
+    console.log(this.request.expirationTime);
     if (Request.isExpired(this.request.expirationTime)) {
       return false;
     }
-    // update db
+
+    // Update the database
     return true;
   }
 }
 
-
-export {
-  User,
-  Request,
-  Approver
-}
+export { User, Request, Approver, TYPES };
